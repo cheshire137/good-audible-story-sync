c94122f784c09389327f65b5752711452855ff0a
Sarah Vessels <cheshire137@gmail.com>
2024-12-18 13:57:19 -0600

    Output user profile info

--- lib/good_audible_story_sync/audible/client.rb
+++ lib/good_audible_story_sync/audible/client.rb
@@ -5,8 +5,22 @@ require "httparty"
 module GoodAudibleStorySync
   module Audible
     class Client
+      class NotAuthenticatedError < StandardError; end
+
+      US_DOMAIN = "com"
+
       def initialize(auth:)
         @auth = auth
+        @api_url = "https://api.audible.#{US_DOMAIN}"
+      end
+
+      def get_user_profile
+        raise NotAuthenticatedError unless @auth.access_token
+        response = HTTParty.get("#{@api_url}/user/profile", headers: headers)
+        unless response.code == 200
+          raise "Error getting user profile (#{response.code}):\n#{response.body}"
+        end
+        JSON.parse(response.body)
       end
 
       def get_library
@@ -15,8 +29,8 @@ module GoodAudibleStorySync
 
       private
 
-      def api_url
-        "https://api.audible.com"
+      def headers
+        { "Authorization" => "Bearer #{@auth.access_token}" }
       end
     end
   end
