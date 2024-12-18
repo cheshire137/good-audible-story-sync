7b4ffa2ff9254cd5a120564b5335288cd86f62ab
Sarah Vessels <cheshire137@gmail.com>
2024-12-18 13:52:36 -0600

    Output device name on success

--- lib/good_audible_story_sync/audible/auth_flow.rb
+++ lib/good_audible_story_sync/audible/auth_flow.rb
@@ -7,12 +7,27 @@ module GoodAudibleStorySync
     class AuthFlow
       def self.call
         audible_auth = Auth.new
-        puts "Audible auth URL: #{audible_auth.oauth_url}"
-        puts "\n\nEnter the URL you were redirected to after logging in:"
+        puts "Please authenticate with Audible via: #{audible_auth.oauth_url}"
+        puts "\nEnter the URL you were redirected to after logging in:"
         url_after_login = gets.chomp
         audible_auth.set_authorization_code_from_oauth_redirect_url(url_after_login)
-        puts "\n\nRegistering Audible device..."
-        device_info = audible_auth.register_device
+
+        puts "\nRegistering Audible device..."
+        success = begin
+          audible_auth.register_device
+        rescue => err
+          puts "Error registering device: #{err}"
+          return
+        end
+
+        unless success
+          puts "\nFailed to authenticate with Audible"
+          return
+        end
+
+        device_name = audible_auth.device_info["device_name"]
+        puts "\nSuccessfully authenticated with Audible and registered device: #{device_name}"
+        audible_auth
       end
     end
   end
