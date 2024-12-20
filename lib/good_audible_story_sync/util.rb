1b4c7f462e89abba3421bf84c8e98fe5f0af9f18
Sarah Vessels <cheshire137@gmail.com>
2024-12-20 15:30:02 -0600

    Add Util#join_words
    Thanks Rails! https://apidock.com/rails/Array/to_sentence

--- lib/good_audible_story_sync/util.rb
+++ lib/good_audible_story_sync/util.rb
@@ -1,7 +1,22 @@
 # frozen_string_literal: true
+# typed: true
 
 module GoodAudibleStorySync
   module Util
+    extend T::Sig
+
+    sig { params(words: T::Array[String]).returns(String) }
+    def self.join_words(words)
+      case words.size
+      when 0 then ""
+      when 1 then T.must(words[0])
+      when 2 then words.join(" and ")
+      else
+        head = T.must(words[0...-1]).join(", ")
+        tail = T.must(words[-1])
+        "#{head}, and #{tail}"
+      end
+    end
   end
 end
 
