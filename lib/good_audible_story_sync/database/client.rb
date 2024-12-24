08c744878bb2c72d8ffcff922400d6be49e4aaeb
Sarah Vessels <cheshire137@gmail.com>
2024-12-24 14:55:01 -0600

    Add sync_times table

--- lib/good_audible_story_sync/database/client.rb
+++ lib/good_audible_story_sync/database/client.rb
@@ -6,6 +6,13 @@ module GoodAudibleStorySync
     class Client
       extend T::Sig
 
+      sig { params(db_file: String, cipher: T.nilable(Util::Cipher)).returns(Client) }
+      def self.setup(db_file, cipher: nil)
+        client = new(db_file: db_file, cipher: cipher)
+        client.create_tables
+        client
+      end
+
       sig { returns SQLite3::Database }
       attr_reader :db
 
@@ -21,6 +28,9 @@ module GoodAudibleStorySync
       sig { returns StorygraphBooks }
       attr_reader :storygraph_books
 
+      sig { returns SyncTimes }
+      attr_reader :sync_times
+
       sig { params(db_file: String, cipher: T.nilable(Util::Cipher)).void }
       def initialize(db_file:, cipher: nil)
         @db = SQLite3::Database.new(db_file)
@@ -28,14 +38,15 @@ module GoodAudibleStorySync
         @credentials = Credentials.new(db_client: self)
         @audible_books = AudibleBooks.new(db: @db)
         @storygraph_books = StorygraphBooks.new(db: @db)
+        @sync_times = SyncTimes.new(db: @db)
       end
 
       sig { void }
       def create_tables
-        puts "#{Util::INFO_EMOJI} Creating database tables..."
         @audible_books.create_table
         @storygraph_books.create_table
         @credentials.create_table
+        @sync_times.create_table
       end
     end
   end
---
+++ lib/good_audible_story_sync/database/sync_times.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+# typed: true
+
+require "date"
+
+module GoodAudibleStorySync
+  module Database
+    class SyncTimes
+      extend T::Sig
+
+      TABLE_NAME = "sync_times"
+
+      sig { params(db: SQLite3::Database).void }
+      def initialize(db:)
+        @db = db
+      end
+
+      sig { params(key: String).returns(T.nilable(DateTime)) }
+      def find(key:)
+        puts "#{Util::INFO_EMOJI} Checking when #{key} was last synced..."
+        timestamp_str = @db.get_first_value("SELECT timestamp FROM #{TABLE_NAME} WHERE key = ?", key)
+        return unless timestamp_str
+
+        DateTime.parse(timestamp_str)
+      end
+
+      sig { void }
+      def create_table
+        puts "#{Util::INFO_EMOJI} Ensuring table #{TABLE_NAME} exists..."
+        @db.execute <<~SQL
+          CREATE TABLE IF NOT EXISTS #{TABLE_NAME} (
+            key TEXT PRIMARY KEY,
+            timestamp TEXT
+          );
+        SQL
+      end
+
+      sig { params(key: String, timestamp: T.nilable(DateTime)).void }
+      def upsert(key:, timestamp:)
+        puts "#{Util::INFO_EMOJI} Saving sync time #{key}..."
+        values = [key, timestamp&.iso8601]
+        @db.execute("INSERT INTO #{TABLE_NAME} (key, timestamp) " \
+          "VALUES (?, ?) ON CONFLICT(key) DO UPDATE SET timestamp=excluded.timestamp", values)
+      end
+    end
+  end
+end
