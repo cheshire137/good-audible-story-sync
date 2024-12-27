d840a13ed68a78b511ca17d6e07f0f27dee6573f
Sarah Vessels <cheshire137@gmail.com>
2024-12-27 13:56:52 -0600

    Prompt user for what to do

---
+++ lib/good_audible_story_sync/input_loop.rb
@@ -0,0 +1,140 @@
+# frozen_string_literal: true
+# typed: true
+
+module GoodAudibleStorySync
+  class InputLoop
+    extend T::Sig
+
+    class UserCommand < T::Enum
+      enums do
+        LoadAudibleLibrary = new("a")
+        LoadAudibleUserProfile = new("u")
+        LoadStorygraphLibrary = new("s")
+        Quit = new("q")
+      end
+    end
+
+    sig { params(script_name: String).void }
+    def self.run(script_name:)
+      new(script_name: script_name).run
+    end
+
+    sig { params(script_name: String).void }
+    def initialize(script_name:)
+      @script_name = script_name
+    end
+
+    sig { void }
+    def run
+      options # parse command line options
+      loop do
+        print_options
+        cmd = get_user_command
+        process_command(cmd)
+      end
+    end
+
+    private
+
+    sig { void }
+    def print_options
+      puts "#{UserCommand::LoadAudibleLibrary.serialize}) load Audible library"
+      puts "#{UserCommand::LoadAudibleUserProfile.serialize}) load Audible user profile"
+      puts "#{UserCommand::LoadStorygraphLibrary.serialize}) load Storygraph library"
+      puts "#{UserCommand::Quit.serialize}) quit"
+    end
+
+    sig { returns UserCommand }
+    def get_user_command
+      cmd = T.let(nil, T.nilable(UserCommand))
+      while cmd.nil?
+        print "Choose an option: "
+        input = gets.chomp
+        cmd = UserCommand.try_deserialize(input)
+        puts "Invalid command" if cmd.nil?
+      end
+      cmd
+    end
+
+    sig { params(cmd: UserCommand).void }
+    def process_command(cmd)
+      case cmd
+      when UserCommand::LoadAudibleLibrary then load_audible_library
+      when UserCommand::LoadAudibleUserProfile then load_audible_user_profile
+      when UserCommand::LoadStorygraphLibrary then load_storygraph_library
+      when UserCommand::Quit then quit
+      else
+        T.absurd(cmd)
+      end
+    end
+
+    sig { void }
+    def load_audible_library
+      library = Audible::Library.load_with_finish_times(client: audible_client,
+        options: options, db_client: db_client)
+      puts library
+    end
+
+    sig { void }
+    def load_storygraph_library
+      library = Storygraph::Library.load(client: storygraph_client, db_client: db_client,
+        options: options)
+      puts library
+    end
+
+    sig { void }
+    def load_audible_user_profile
+      puts "#{Util::INFO_EMOJI} Getting Audible user profile..."
+      user_profile = audible_client.get_user_profile
+      puts user_profile.to_s(indent_level: 1)
+    end
+
+    sig { void }
+    def quit
+      puts "Goodbye!"
+      exit 0
+    end
+
+    sig { returns Audible::Client }
+    def audible_client
+      @audible_client ||= Audible::Client.new(auth: audible_auth, options: options,
+        credentials_db: db_client.credentials)
+    end
+
+    sig { returns Audible::Auth }
+    def audible_auth
+      return @audible_auth if @audible_auth
+      maybe_auth = GoodAudibleStorySync::Audible::AuthFlow.run(db_client: db_client)
+      exit 1 if maybe_auth.nil?
+      @audible_auth = maybe_auth
+    end
+
+    sig { returns Storygraph::Client }
+    def storygraph_client
+      @storygraph_client ||= Storygraph::Client.new(auth: storygraph_auth)
+    end
+
+    sig { returns Storygraph::Auth }
+    def storygraph_auth
+      return @storygraph_auth if @storygraph_auth
+      maybe_auth = Storygraph::AuthFlow.run(db_client: db_client)
+      exit 1 if maybe_auth.nil?
+      @storygraph_auth = maybe_auth
+    end
+
+    sig { returns Database::Client }
+    def db_client
+      @db_client ||= Database::Client.load(options.database_file, cipher: cipher)
+    end
+
+    sig { returns Options }
+    def options
+      @options ||= Options.parse(script_name: @script_name, cipher: cipher)
+    end
+
+    sig { returns Util::Cipher }
+    def cipher
+      @cipher ||= Util::Cipher.new
+    end
+  end
+end
