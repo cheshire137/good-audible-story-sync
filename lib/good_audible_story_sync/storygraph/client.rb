c243e8c189610a222264b7db7f43da01046a949c
Sarah Vessels <cheshire137@gmail.com>
2024-12-27 15:13:52 -0600

    Pull out #get, allow loading just 1 page

--- lib/good_audible_story_sync/storygraph/client.rb
+++ lib/good_audible_story_sync/storygraph/client.rb
@@ -18,15 +18,20 @@ module GoodAudibleStorySync
         @auth = auth
       end
 
-      # e.g., https://app.thestorygraph.com/books-read/cheshire137
-      sig { returns Library }
-      def get_read_books
-        url = "#{BASE_URL}/books-read/#{@auth.username}"
+      sig { params(path: String).returns(Mechanize::Page) }
+      def get(path)
+        url = "#{BASE_URL}#{path}"
         puts "#{Util::INFO_EMOJI} GET #{url}"
-        first_page = load_page(-> { @agent.get(url) })
+        load_page(-> { @agent.get(url) })
+      end
+
+      # e.g., https://app.thestorygraph.com/books-read/cheshire137
+      sig { params(page: Integer, load_all_pages: T::Boolean).returns(Library) }
+      def get_read_books(page: 1, load_all_pages: true)
+        initial_page = get("/books-read/#{@auth.username}?page=#{page}")
 
         filter_header_prefix = "Filter list "
-        filter_header_el = first_page.search(".filter-menu *").detect do |el|
+        filter_header_el = initial_page.search(".filter-menu *").detect do |el|
           el.text.start_with?(filter_header_prefix)
         end
         total_books = if filter_header_el
@@ -34,7 +39,7 @@ module GoodAudibleStorySync
         end
 
         library = Library.new(total_books: total_books)
-        books = get_read_books_on_each_page(page: first_page)
+        books = get_read_books_on_page(page: initial_page, load_all_pages: load_all_pages)
         books.each { |book| library.add_book(book) }
         library
       end
@@ -62,19 +67,22 @@ module GoodAudibleStorySync
         raise "No search query provided" if query.strip.empty?
 
         params = { "search_term" => query }
-        url = "#{BASE_URL}/search?#{URI.encode_www_form(params)}"
-        puts "#{Util::INFO_EMOJI} GET #{url}"
-        page = load_page(-> { @agent.get(url) })
+        page = get("/search?#{URI.encode_www_form(params)}")
         T.let(page.search("#search-results-ul li a"), Nokogiri::XML::NodeSet)
       end
 
-      sig { params(url: T.nilable(String), page: T.nilable(Mechanize::Page)).returns(T::Array[Book]) }
-      def get_read_books_on_each_page(url: nil, page: nil)
-        if url
-          puts "#{Util::INFO_EMOJI} GET #{url}"
-          page = load_page(-> { @agent.get(url) })
+      sig do
+        params(
+          path: T.nilable(String),
+          page: T.nilable(Mechanize::Page),
+          load_all_pages: T::Boolean
+        ).returns(T::Array[Book])
+      end
+      def get_read_books_on_page(path: nil, page: nil, load_all_pages: true)
+        if path
+          page = get(path)
         elsif page.nil?
-          raise "Either a URL or a page must be provided"
+          raise "Either a relative URL or a page must be provided"
         end
 
         book_elements = T.let(page.search(".read-books-panes .book-pane"), Nokogiri::XML::NodeSet)
@@ -82,12 +90,15 @@ module GoodAudibleStorySync
           Book.from_read_book(book_element, base_url: BASE_URL)
         end
 
-        next_page_link = page.at(".read-books #next_link")
-        if next_page_link
-          units = books.size == 1 ? "book" : "books"
-          puts "#{Util::INFO_EMOJI} Found #{books.size} #{units} on page"
+        if load_all_pages
+          next_page_link = page.at(".read-books #next_link")
+          if next_page_link
+            units = books.size == 1 ? "book" : "books"
+            puts "#{Util::INFO_EMOJI} Found #{books.size} #{units} on page"
 
-          books += get_read_books_on_each_page(url: BASE_URL + next_page_link["href"])
+            books += get_read_books_on_page(path: next_page_link["href"],
+              load_all_pages: load_all_pages)
+          end
         end
 
         books
