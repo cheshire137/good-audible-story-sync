3d320c4b4da2747c619dfb218b60ed1bbf95e4c7
Sarah Vessels <cheshire137@gmail.com>
2025-01-04 11:55:21 -0600

    Use old user agent to avoid cookie error

--- lib/good_audible_story_sync/goodreads/auth.rb
+++ lib/good_audible_story_sync/goodreads/auth.rb
@@ -34,6 +34,8 @@ module GoodAudibleStorySync
       sig { params(agent: T.nilable(Mechanize), data: T::Hash[String, T.nilable(String)]).void }
       def initialize(agent: nil, data: {})
         @agent = agent || Mechanize.new
+        # https://www.zytrax.com/tech/web/browser_ids.htm
+        @agent.user_agent = "Mozilla/5.0 (iPod; U; CPU iPhone OS 2_2_1 like Mac OS X; en-us) AppleWebKit/525.18.1 (KHTML, like Gecko) Version/3.1.1 Mobile/5H11a Safari/525.20"
         @email = data["email"]
         @password = data["password"]
       end
